622fd4bed0e117869d07f2c41eee08deeeb14f7c
saii-coo <123abbamusa123@gmail.com>
2026-04-26 17:20:05 +0100

    Create translate.rb

---
+++ lib/plugins/translate.rb
@@ -0,0 +1,43 @@
+require "httparty"
+
+module Telegem
+  module Plugins
+    class Translate
+      def initialize(word, from, to)
+        @word = word
+        @from = from
+        @to = to
+        start_translating
+      end 
+
+      def start_translating
+        url = "https://zen-drx-api.onrender.com"
+        options = {
+          body: {
+            word: @word,
+            from: @from,
+            to: @to
+          }.to_json,
+          headers: {
+            'Content-Type' => 'application/json'
+          }
+        }
+        response = HTTParty.post(url, options)
+        if response.success?
+          translation = response.parsed_response["translation"]
+           {
+             error: "false",
+             translation: translation
+           }
+        else 
+          {
+            error: "an error occurred",
+            code: "#{response.code}"
+          }
+        end 
+      end 
+    end 
+  end
+end 
+          
+            
